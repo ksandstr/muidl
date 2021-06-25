1fc3d8cba6517cc40391a5466d384d5722a5323a
Kalle A. Sandström <ksandstr@iki.fi>
2021-06-25 07:53:24 +0300

    modernize remove-unwanted-opts.pl

--- remove-unwanted-opts.pl
+++ remove-unwanted-opts.pl
@@ -1,18 +1,15 @@
 #!/usr/bin/perl -w
-use strict;
+use Modern::Perl '2020';
 
-# llvm-config puts optimization, linkage, and warning things in our command
-# line. that won't do. off with their heads.
-my %skips = map { ($_, $_) }
-	qw/-DNDEBUG -D_GNU_SOURCE -pedantic -Wpedantic/;
+# llvm-config used to put optimization, linkage, and warning things in our
+# command line. can't have that. off with their heads should they reappear.
 
+my %skips = map { $_ => 1 } qw/-DNDEBUG -D_GNU_SOURCE -pedantic -Wpedantic/;
 while(<>) {
-	my @bits = split /\s+/;
-	my @out = ();
-	foreach (@bits) {
-		next if exists($skips{$_});
-		next if /^-f/ && !($_ eq '-fPIC');
-		next if /^-O/;
+	my @out;
+	foreach (split /\s+/) {
+		next if exists $skips{$_};	# fuck these ones in particular
+		next if /^-O/ or (/^-f/ && $_ ne '-fPIC');	# these ones, categorically
 		push @out, $_;
 	}
 	print join(" ", @out) . "\n";
